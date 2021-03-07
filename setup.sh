c1413c35d43a964e710f4623d3124145cc0d0983
tigranb2 <59093401+tigranb2@users.noreply.github.com>
2021-03-06 22:37:35 -0500

    install geth + mininet + cassandra

--- setup.sh
+++ setup.sh
@@ -48,6 +48,59 @@ cd ..
 mininet/util/install.sh -a
 sudo mn --test pingall
 
+cd
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y build-essential linux-headers-$(uname -r)
+sudo apt install -y git zip python3-pip
+
+
+#sudo add-apt-repository -y ppa:ethereum/ethereum
+#sudo apt-get update
+#sudo apt-get install -y ethereum
+
+cd
+wget -q https://dl.google.com/go/go1.14.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.14.linux-amd64.tar.gz
+rm go1.14.linux-amd64.tar.gz
+echo 'export PATH=$PATH:/usr/local/go/bin' >> /root/.bashrc
+echo 'export GOPATH=~/go' >> /root/.bashrc
+source /root/.bashrc
+
+cd
+wget -q https://github.com/ethereum/go-ethereum/archive/v1.9.12.tar.gz
+tar -xzf v1.9.12.tar.gz
+rm v1.9.12.tar.gz
+cd go-ethereum-1.9.12
+make all
+
+cd
+echo 'export PATH=$PATH:~/go-ethereum-1.9.12/build/bin' >> /root/.bashrc
+source /root/.bashrc
+
+
+cd
+mkdir mininet
+cd mininet
+git clone git://github.com/mininet/mininet
+cd mininet
+git checkout -b 2.2.2
+cd ..
+mininet/util/install.sh -a
+sudo mn --test pingall
+
+cd
+sudo apt install openjdk-8-jre-headless
+echo 'JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64' >> /root/.bashrc
+source /root/.bashrc
+
+cd
+echo "deb http://downloads.apache.org/cassandra/debian 40x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+curl https://downloads.apache.org/cassandra/KEYS | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install cassandra
+
+
 #cd
 #mkdir -p ethData1/keystore
 #mkdir -p ethData2/keystore
